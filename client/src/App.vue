6a53dc17df6a19bd3a841b4a516ae3a0877463e9
max gravitt <max.gravitt@gmail.com>
2019-01-13 13:46:16 -0500

    changed to balance

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,7 +12,7 @@
             <v-icon>attach_money</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>View Balance</v-list-tile-title>
+            <v-list-tile-title>Balance</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
 <!-- 
