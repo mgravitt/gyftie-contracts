0a843143a83ff5e48365cce6596269d28720a7a3
Max G <max@Maxs-MacBook-Pro.local>
2019-02-12 11:47:24 -0500

    updated gyft benefit 15% for < 10, 10% < 100, and 5% < 1000

--- genesisgen/genesisgen.cpp
+++ genesisgen/genesisgen.cpp
@@ -17,11 +17,11 @@ ACTION genesisgen::generate (   name from,
     double gyft_benefit= 0.0;
 
     if (gyfter_gft_balance  <= (one_gyftie_token * 10)) {
-        gyft_benefit = 0.3;
+        gyft_benefit = 0.15;
     } else if (gyfter_gft_balance  <= (one_gyftie_token * 100)) {
-        gyft_benefit = 0.2;
+        gyft_benefit = 0.10;
     } else if (gyfter_gft_balance  <= (one_gyftie_token * 1000)) {
-        gyft_benefit = 0.1;
+        gyft_benefit = 0.05;
     } else if (gyfter_gft_balance  <= (one_gyftie_token * 10000)) {
         gyft_benefit = 0.03;
     } else if (gyfter_gft_balance  <= (one_gyftie_token * 100000)) {
